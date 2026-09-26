3cfbe34dea7ddb217c63414d15d5bbf3c9f2da5a
Arzam18 <98748640+Arzam18@users.noreply.github.com>
2026-09-26 10:20:49 +0800

    Add files via upload

--- src/search.cpp
+++ src/search.cpp
@@ -1,4 +1,5 @@
 #include "search.h"
+#include "threads.h"
 
 Searcher::Searcher(Board board, const std::vector<Move> &move_list, TranspositionTable &transposition_table, ThreadData &thread_data, ThreadManager &thread_manager, uint32_t age, bool is_main_thread) : board(board), transposition_table(transposition_table), thread_data(thread_data), thread_manager(thread_manager)
 {
